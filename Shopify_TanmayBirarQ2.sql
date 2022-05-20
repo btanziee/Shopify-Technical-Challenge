827ecb8aa733f570e98e9d1646a180c8f2f1a5c6
btanziee <97150110+btanziee@users.noreply.github.com>
2022-05-20 12:16:00 -0700

    Update Shopify_TanmayBirarQ2.sql

--- Shopify_TanmayBirarQ2.sql
+++ Shopify_TanmayBirarQ2.sql
@@ -1,16 +1,17 @@
-#Q2 - 1
-#ANS: 54
+#Q2 a.
 SELECT ShipperName, COUNT(O.ShipperID)
 FROM Orders O
 INNER JOIN Shippers S ON O.ShipperID = S.ShipperID 
 WHERE S.ShipperName = "Speedy Express"
 GROUP BY S.ShipperName;
 
+b.
 SELECT TOP 1 E.LastName FROM Employees E
 INNER JOIN Orders O on O.EmployeeID = E.EmployeeID
 Group by E.LastName
 Order by COUNT(O.EmployeeID) DESC;
 
+c.
 Select TOP 1 Products.ProductName, Customers.Country, COUNT(Orders.OrderID) AS Tot_Orders FROM 
 (((Customers
 INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID)
